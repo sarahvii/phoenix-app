b3880ae2236ed2d4c4ef41a04bf06e2106d97eb1
WaxenOsprey <psc000001@gmail.com>
2023-07-11 01:35:14 -0700

    minor changes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -35,75 +35,64 @@ const createRouter = function (collection) {
 
 // CREATE
 router.post('/', (req, res) => {
-    const { ticker, order, stockName } = req.body;
-  
-    console.log('Received new order:', order, ticker, stockName);
-  
-    // Find the stock by ticker
-    collection
-      .findOne({ ticker })
-      .then(stock => {
-        if (stock) {
-          console.log('Existing stock found:', stock);
-  
-          // Add the new order to the stock's orders array
-          stock.orders.push(order);
-  
-          console.log('Updated stock with new order:', stock);
-  
-          // Calculate the new totalShares value
-          stock.totalShares = calculateTotalShares(stock);
-  
-          console.log('Total shares:', stock.totalShares);
-  
-          // Update the stock in the database
-          collection
-            .updateOne(
-                { _id: stock._id }, 
-                {
-                $set: {
-                  orders: stock.orders,
-                  totalShares: stock.totalShares
-                }
+  const { ticker, order, stockName } = req.body;
+
+  // Find the stock by ticker
+  collection
+    .findOne({ ticker })
+    .then(stock => {
+      if (stock) {
+        //If the stock already exists, update it
+        stock.orders.push(order);
+
+        // Calculate the new totalShares value
+        stock.totalShares = calculateTotalShares(stock);
+
+        // Update the stock in the database
+        collection
+          .updateOne(
+            { _id: stock._id },
+            {
+              $set: {
+                orders: stock.orders,
+                totalShares: stock.totalShares
               }
-            )
-            .then(result => {
-              console.log('Update result:', result);
-              res.json({ message: 'Stock updated successfully.' });
-            })
-            .catch(error => {
-              console.error('Update error:', error);
-              res.status(500).json({ message: 'Failed to update stock.' });
-            });
-        } else {
-          // Create a new stock
-          const newStock = {
-            stockName: stockName,
-            ticker: ticker,
-            orders: [order],
-            totalShares: order.sharesQuantity
-          };
-  
-          console.log('New stock:', newStock);
-  
-          // Insert the new stock into the database
-          collection
-            .insertOne(newStock)
-            .then(result => {
-              console.log('Insert result:', result);
-              res.json({ message: 'Stock created successfully.' });
-            })
-            .catch(error => {
-              console.error('Insert error:', error);
-              res.status(500).json({ message: 'Failed to create stock.' });
-            });
-        }
-      })
-      .catch(error => {
-        console.error('Find error:', error);
-        res.status(500).json({ message: 'Failed to find stock.' });
-      });
-  });
+            }
+          )
+          .then(result => {
+            res.json({ message: 'Stock updated successfully.' });
+          })
+          .catch(error => {
+            console.error('Update error:', error);
+            res.status(500).json({ message: 'Failed to update stock.' });
+          });
+      } else {
+        // Create a new stock
+        const newStock = {
+          stockName: stockName,
+          ticker: ticker,
+          orders: [order],
+          totalShares: order.sharesQuantity
+        };
+
+        // Insert the new stock into the database
+        collection
+          .insertOne(newStock)
+          .then(result => {
+            res.json({ message: 'Stock created successfully.' });
+          })
+          .catch(error => {
+            console.error('Insert error:', error);
+            res.status(500).json({ message: 'Failed to create stock.' });
+          });
+      }
+    })
+    .catch(error => {
+      console.error('Find error:', error);
+      res.status(500).json({ message: 'Failed to find stock.' });
+    });
+});
+
   
   
   
