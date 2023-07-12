dcaae674fe3646cbd3fd7c8ab0cfb837b821fecf
Sarah Smith <sarahsmithvii@gmail.com>
2023-07-12 20:05:03 +0100

    refactored detail

--- client/src/components/CompanyProfileService.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://finnhub.io/api/v1/stock/";
-
-export default {
-  detail
-}
-
-function detail(symbol) {
-  return fetch(`${BASE_URL}profile2?symbol=${symbol}&token=cim0421r01qucvvrg00gcim0421r01qucvvrg010`).then((res) => res.json());
-}
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-// import "./styles.css";
-import CompanyProfileService from "./CompanyProfileService";
+
+const BASE_URL = "https://finnhub.io/api/v1/stock/";
 
 export default function SearchBar() {
   const [symbol, setSymbol] = useState('');
   const [details, setDetails] = useState(null);
 
+  const detail = (symbol) => {
+    return fetch(`${BASE_URL}profile2?symbol=${symbol}&token=cim0421r01qucvvrg00gcim0421r01qucvvrg010`).then((res) => res.json());
+  }
+
   const handleClick = async () => {
     try {
-    const response = await CompanyProfileService.detail(symbol);
+    const response = await detail(symbol);
     console.log(response);
     setDetails(response);
   } catch (err) {
