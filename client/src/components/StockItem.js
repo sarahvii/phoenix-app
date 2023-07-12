ebfff8c3bf5b018f36b63c279f5f223b27128cfc
WaxenOsprey <psc000001@gmail.com>
2023-07-11 22:30:49 -0700

    added rendering of db stocks and fetching of live price and company data apis inside StockItem for display

--- client/src/components/StockItem.js
+++ client/src/components/StockItem.js
@@ -4,18 +4,43 @@ import { useEffect, useState } from "react";
 
 const StockItem = ({ stock }) => {
   const [livePriceData, setLivePriceData] = useState(null);
+  const [liveCompanyData, setLiveCompanyData] = useState(null);
 
   const StockItemDiv = styled.div`
     border: 5px solid black;
     margin: 10px;
-    padding: 10px;
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   `;
 
-  const StockName = styled.h3``;
+  const StockName = styled.h3`
+    font-size: 20px;
+    `;
 
-  const StockTicker = styled.p``;
 
-  const StockTotalShares = styled.p``;
+  const StockTicker = styled.p`
+    font-size: 15px;
+    `;
+
+  const StockTotalShares = styled.p`
+    font-size: 15px;
+`;
+
+  const StockCurrentPrice = styled.p`
+    font-size: 15px;
+    `;
+
+  const StockTotalValue = styled.p`
+  `;
+
+  const Logo = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    `;
 
   useEffect(() => {
     fetch(
@@ -25,12 +50,27 @@ const StockItem = ({ stock }) => {
       .then((data) => setLivePriceData(data));
   }, []);
 
+  useEffect(() => {
+    fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${stock.ticker}&token=cim0421r01qucvvrg00gcim0421r01qucvvrg010`
+    )
+        .then((res) => res.json())
+        .then((data) => setLiveCompanyData(data));
+    }, []);
+
+    if (!liveCompanyData) {
+        return ("Loading...")
+      }
+
+    const {logo} = liveCompanyData;
+
   return (
     <StockItemDiv>
-        <h3>{stock.ticker}</h3>
-        <p>{stock.stockName}</p>
-        <p>{stock.totalShares}</p>
-        <p>{"Current Price: " + livePriceData.c}</p>
+        <Logo src={logo} alt="company logo" />
+        <StockTicker>{stock.ticker}</StockTicker>
+        <StockName>{liveCompanyData.name}</StockName>
+        <StockTotalShares>{stock.totalShares}</StockTotalShares>
+        <StockCurrentPrice>{"Current Price: " + livePriceData.c}</StockCurrentPrice>
+
     </StockItemDiv>
     
   );
