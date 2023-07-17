3c1a207b9b7264aa34ab33e5292c0478cc917228
Sarah Smith <sarahsmithvii@gmail.com>
2023-07-17 19:20:46 +0100

    styling improved

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -44,21 +44,57 @@ const Modal = ({ open, onClose, details, handleYesClick, setSelectedStock }) => 
 };
 
 const ModalOverlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  width: 100%;
-  height: 100%;
+background-color: rgba(0, 0, 0, 0.5);
+position: fixed;
+width: 100%;
+height: 100%;
+top: 0;
+left: 0;
+display: flex;
+justify-content: center;
+align-items: center;
 `;
+
 const ModalContainer = styled.div`
-  max-width: 600px;
-  width: 100%;
-  position: fixed;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+background-color: #fff;
+padding: 20px;
+border-radius: 8px;
+box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.2);
+max-width: 400px;
+width: 90%;
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+p {
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 20px;
+  border-radius: 4px;
+}
+
+.buttonContainer {
   display: flex;
-  background-color: #ffffff;
-  box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.75);
+  justify-content: flex-end;
+}
+
+button {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-left: 10px;
+  font-size: 16px;
+}
 `;
 
 export default Modal;
